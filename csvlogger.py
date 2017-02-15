45a2bfd739a1ea7bf52c3201ccaca0f69b1d4eb4
jimholladay <jimholladaync@gmail.com>
2017-02-14 20:59:59 -0500

    Linted csvlogger.py

--- csvlogger.py
+++ csvlogger.py
@@ -1,34 +1,35 @@
-#===============================================================================
+# ===============================================================================
 # csvlogger.py for FilePusher
-# 
+#
 # This holds the queue and the process to write to the csv file.
 #
 # for use with python 3.5
-#===============================================================================
+# ===============================================================================
+""" This module holds the thread for adding items to the csv log."""
 import datetime
 import traceback
 from time import sleep
 from multiprocessing import Queue
 from threading import Thread
-from config.log_settings import log2log
-from config.log_settings import log2log
 
-csv_log_queue = Queue(100)
+from .config.log_settings import log2log
+
+_CSV_LOG_QUEUE = Queue(100)
+
 
 class CSVLogThread(Thread):
-    ''' This class writes to the CSV file. '''
-    def __init__(self, group = None, target = None, name = None, args = (), 
-                 kwargs = None, daemon = None):
+    """ This class writes to the CSV file. """
+    def __init__(self):
         super(CSVLogThread, self).__init__()
-    
+
     def run(self):
         while True:
-            try:                
-                if not csv_log_queue.empty():
-                    datename = datetime.date.today().strftime('%Y-%m-%d')
-                    with open("z:/Archive/" + datename + ".csv", 'a') as csvfile:
-                        line = csv_log_queue.get()
-                        csvfile.write(line)
+            try:
+                if not _CSV_LOG_QUEUE.empty():
+                    date_name = datetime.date.today().strftime('%Y-%m-%d')
+                    with open("z:/Archive/" + date_name + ".csv", 'a') as csv_file:
+                        line = _CSV_LOG_QUEUE.get()
+                        csv_file.write(line)
                         sleep(1)
             except Exception as ex:
                 log2log('debug', "CSV Log exception " + str(ex))
