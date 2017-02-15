2d3eb7a159705ba8a91de430b0a099f272e8a828
jimholladay <jimholladaync@gmail.com>
2017-02-15 11:08:14 -0500

    Linted filepusher.py

--- filepusher.py
+++ filepusher.py
@@ -1,38 +1,38 @@
-﻿#------------------------------------------------------------------------------
+﻿# ------------------------------------------------------------------------------
 # This is the main runner for FilePusher.
 #     for use with python 3.5
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+""" This is the main runner of Filepusher."""
 import time
 import sys
 import datetime
-from pre_copy_thread import ImportValidationThread
-from copy_thread import ProducerThread
-from post_copy_thread import PostProducerThread
-from config.log_settings import log2log
-from csvlogger import csv_log_queue, CSVLogThread
+
+from .pre_copy_thread import ImportValidationThread
+from .copy_thread import ProducerThread
+from .post_copy_thread import PostProducerThread
+from .config.log_settings import log2log
+from .csvlogger import CSVLogThread
 
 if __name__ == "__main__":
     try:
         # create each thread for all the processes
-        thread1 = ImportValidationThread(name='validation')
-        thread2 = ProducerThread(name='producer')
-        thread3 = PostProducerThread(name='postproducer')
-        thread4 = CSVLogThread(name='csvlog')
-        thread1.start()
-        thread2.start()
-        thread3.start()
-        thread4.start()
+        THREAD1 = ImportValidationThread(name='validation')
+        THREAD2 = ProducerThread(name='producer')
+        THREAD3 = PostProducerThread(name='post_producer')
+        THREAD4 = CSVLogThread(name='csv_log')
+        THREAD1.start()
+        THREAD2.start()
+        THREAD3.start()
+        THREAD4.start()
 
         while True:
-            print(datetime.datetime.today().strftime('%Y-%m-%d %H:%M') + 
+            print(datetime.datetime.today().strftime('%Y-%m-%d %H:%M') +
                   " Still Working")
             time.sleep(600)
-            
+
     except KeyboardInterrupt:
         sys.exit()
-        
+
     except Exception as ex:
         log2log("error", "Main Exception... " + str(ex))
         log2log("error", str(sys.exc_info()))
-        
-    
