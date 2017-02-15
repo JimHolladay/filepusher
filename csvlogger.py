d9ff7924e8daf2e38b1afa4dd041d4c931107f70
jimholladay <jimholladaync@gmail.com>
2017-02-15 11:37:57 -0500

    csvlogger queue now uses a dictionary.

--- csvlogger.py
+++ csvlogger.py
@@ -30,9 +30,25 @@ class CSVLogThread(Thread):
                     date_name = datetime.date.today().strftime('%Y-%m-%d')
                     with open("z:/Archive/" + self.name + "_" + date_name + ".csv", 'a') \
                             as csv_file:
-                        line = _CSV_LOG_QUEUE.get()
+                        dictionary = _CSV_LOG_QUEUE.get()
+                        line = dictionary['process_status'] + ", " + \
+                            dictionary['source_folder'] + ", " + \
+                            dictionary['item_name'] + ", " + \
+                            dictionary['object_size'] + ", " + \
+                            dictionary['target_name'] + ", " + \
+                            dictionary['finish_time']
                         csv_file.write(line)
                         sleep(1)
             except Exception as ex:
                 log2log('debug', "CSV Log exception " + str(ex))
                 log2log('debug', traceback.format_exc())
+
+
+source_folder, item.name, statinfo.st_size, item.target['Name'], item.finish_time
+
+csv_dict = {'process_status': "Finished",
+            'source_folder': source_folder,
+            'item_name': item.name,
+            'object_size': statinfo.st_size,
+            'target_name': item.target['Name'],
+            'finish_time': item.finish_time}
