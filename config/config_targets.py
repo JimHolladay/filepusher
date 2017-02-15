a3f7128cbdcc87b4818f40b93523bb3016b46bc3
jimholladay <jimholladaync@gmail.com>
2017-02-15 17:10:55 -0500

    Fixed and linted the config ingest.

---
+++ config/config_targets.py
@@ -0,0 +1,63 @@
+# ===============================================================================
+# config_targets.py for FilePusher
+#
+# This validates and parses the target config file.
+#
+# for use with python 3.4.3
+# ===============================================================================
+""" This module marshalls the contents of targets.yml config file into an
+object with many attributes. """
+import os
+import sys
+import yaml
+
+from .log_settings import log2log
+
+DIRECTORY = r'.\config'
+CONF_FILE = 'targets.yml'
+FULL_PATH = os.path.join(DIRECTORY, CONF_FILE)
+
+
+class TargetsConf(object):
+    """ Class that reads in the target config file. """
+    target_list = []
+
+    def __init__(self):
+        self.target_list = []
+
+    def config_ingest(self):
+        """ Read the config file, acquire info. """
+        if config_probe():
+            try:
+                with open(FULL_PATH, 'r') as conf:
+                    target_config = yaml.load(conf)
+                    for item in target_config['Targets']:
+                        if item['Active'] is True:
+                            self.target_list.append(item)
+            except OSError as ose:
+                log2log("error", 'Config ingest OSError: ' + str(ose))
+            except Exception:
+                log2log("error", 'Config ingest Unexpected error:' + sys.exc_info()[0])
+        else:
+            log2log("error", "There was an issue with the target config file.")
+        return self.target_list
+
+
+def config_probe():
+    """ Test for existence of conf file. If found, is it a file?. """
+    b_return = False
+    if not os.path.exists(FULL_PATH):
+        log2log("error", DIRECTORY + "\\" + CONF_FILE + " doesn't exist.")
+    elif not os.path.isfile(FULL_PATH):
+        log2log("error", DIRECTORY + "\\" + CONF_FILE + " isn't a file.")
+    else:
+        b_return = True
+    return b_return
+
+if __name__ == '__main__':
+    TEST_OBJ = TargetsConf()
+    config_probe()
+    TEST_OBJ.config_ingest()
+    print('\n')
+    print(vars(TEST_OBJ))
+    print('\n')
--- config/log_settings.py
+++ config/log_settings.py
@@ -1,46 +1,44 @@
-#===============================================================================
+# ===============================================================================
 # log_settings.py for FilePusher
 #
 # This is the class that controls the log config settings and ingest..
 #
 # for use with python 3.5
-#===============================================================================
-import yaml
+# ===============================================================================
+""" This is the wrapper for the logger. """
 import logging
-import os
-import socket
 import datetime
-import time
-from pip._vendor.cachecontrol._cmd import setup_logging
 
-def log2log(logtype, msg):
+
+def log2log(log_type, msg):
+    """ Wrapper for the logging module. """
     try:
-        debuglog = logging.getLogger('full-log')
-        logfilename = 'Z:/Logs/filepusher/log-' + datetime.date.today().strftime('%Y-%m-%d') + ".txt"
-        debugloghandler = logging.handlers.RotatingFileHandler(filename=logfilename,
-                                                               maxBytes=500000,
-                                                               backupCount=365,
-                                                               encoding='utf8')
-        debugloghandler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s'))
-        if not debuglog.hasHandlers():
-            debuglog.addHandler(debugloghandler)
-        debuglog.setLevel(logging.DEBUG)
-        if logtype == 'debug':
-            debuglog.debug(msg)
-        elif logtype == 'info':
-            debuglog.info(msg)
-        elif logtype == 'warning':
-            debuglog.warning(msg)
-        elif logtype == 'error':
-            debuglog.error(msg)
+        debug_log = logging.getLogger('full-log')
+        log_file_name = 'Z:/Logs/filepusher/log-' + \
+                        datetime.date.today().strftime('%Y-%m-%d') + \
+                        ".txt"
+        debug_log_handler = logging.handlers.RotatingFileHandler(filename=log_file_name,
+                                                                 maxBytes=500000,
+                                                                 backupCount=365,
+                                                                 encoding='utf8')
+        debug_log_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s'))
+        if not debug_log.hasHandlers():
+            debug_log.addHandler(debug_log_handler)
+        debug_log.setLevel(logging.DEBUG)
+        if log_type == 'debug':
+            debug_log.debug(msg)
+        elif log_type == 'info':
+            debug_log.info(msg)
+        elif log_type == 'warning':
+            debug_log.warning(msg)
+        elif log_type == 'error':
+            debug_log.error(msg)
         else:
             print("Error in log generation.")
-        debugloghandler.close()
-        debuglog.removeHandler(debugloghandler)
+        debug_log_handler.close()
+        debug_log.removeHandler(debug_log_handler)
     except Exception as ex:
         print("Log2Log Exception happened: " + str(ex))
-        
+
 if __name__ == '__main__':
-    log2csv("Started", "Movies", "this_is_a_file.txt", 987654321, "Vidstorage", 
-            datetime.datetime.now())
     log2log('debug', "This is just a test.")
